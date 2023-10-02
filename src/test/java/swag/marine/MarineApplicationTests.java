a9b5be612617d9ddafb9357fc8c9bc26f950e508
jaewoong-gwon <wodndrnjs@naver.com>
2023-10-02 17:00:51 +0900

    basic file upload

---
+++ src/test/java/swag/marine/MarineApplicationTests.java
@@ -0,0 +1,13 @@
+package swag.marine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
