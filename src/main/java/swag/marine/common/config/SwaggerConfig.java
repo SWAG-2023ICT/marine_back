fcd2ba2f2aa45f9cfff9d274f2da5d3452e1ef85
jaewoong-gwon <wodndrnjs@naver.com>
2023-10-02 17:57:14 +0900

    Springdoc - swagger2 사용 - 'org.springdoc:springdoc-openapi-ui:1.7.0' 의존성 추가 - Swagger config class 생성

---
+++ src/main/java/swag/marine/common/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package swag.marine.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI marineAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("project marine API 명세서")
+                        .description("TEST 및 API 명세서로 사용할 예정"));
+    }
+}
