196f04a91fcd51bf3d6672d6dfd3c790de33b889
JaeUng <wodndrnjs@naver.com>
2023-10-03 19:01:57 +0900

    Spring Security 추가 - application.properties swagger path 수정 - security swagger pattern 추가

---
+++ src/main/java/swag/marine/common/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package swag.marine.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    private final String[] SWAGGER_URL = {
+        "/swagger-ui/**",
+        "/marine/**"
+    };
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(SWAGGER_URL).permitAll();
+
+
+        return httpSecurity.build();
+    }
+
+
+}
