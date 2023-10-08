9c37d9eedf50e4341ca0442c53d84de2ac006e94
JaeUng <wodndrnjs@naver.com>
2023-10-08 15:47:43 +0900

    Destination 삭제

--- src/main/java/swag/marine/mapper/DestinationMapper.java
+++
@@ -1,9 +0,0 @@
-package swag.marine.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import swag.marine.model.Destination;
-
-@Mapper
-public interface DestinationMapper {
-    Integer addDestination(Destination destination);
-}
--- src/main/java/swag/marine/service/DestinationService.java
+++
@@ -1,7 +0,0 @@
-package swag.marine.service;
-
-import swag.marine.model.Destination;
-
-public interface DestinationService {
-    boolean addDestination(Destination destination);
-}
