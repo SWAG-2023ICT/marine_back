62eff606c1908ef182808582fce8fba58310d07c
JaeUng <wodndrnjs@naver.com>
2023-10-09 15:07:18 +0900

    Product image 필드 type 변경 - Blob -> byte[]

--- src/main/java/swag/marine/model/Product.java
+++ src/main/java/swag/marine/model/Product.java
@@ -9,7 +9,7 @@ public class Product {
     private int product_id;
     private String origin;
     private String product_name;
-    private Blob product_image; // blob 처리를 어떻게 하면 좋을지 모르겠네요...
+    private byte[] product_image; // blob 처리를 어떻게 하면 좋을지 모르겠네요...
     private String description;
     private int product_status;
     private int store_id;
