90cbe0fc5ca65b1aa3988c71b3f96fe60a49509c
JaeUng <wodndrnjs@naver.com>
2023-10-11 15:22:17 +0900

    Swagger api doc 작성중

--- src/main/java/swag/marine/controller/DestinationController.java
+++ src/main/java/swag/marine/controller/DestinationController.java
@@ -1,7 +1,13 @@
 package swag.marine.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import swag.marine.model.Destination;
@@ -9,11 +15,20 @@ import swag.marine.service.DestinationService;
 
 @RequestMapping("/marine/destination")
 @RestController
+@Tag(name = "destination",description = "주소 관련 API")
 public class DestinationController {
     @Autowired
     DestinationService destinationService;
 
-    @PostMapping("/addDestination")
+    @Operation(summary = "주소 추가",description = "사용자가 주소를 추가합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "주소가 정상적으로 추가되었습니다.",
+                content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400",description = "주소추가에 실패했습니다.",
+                content = @Content(mediaType = "application/json"))
+    })
+    @PostMapping(value = "/addDestination",consumes = MediaType.APPLICATION_JSON_VALUE
+        ,produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity addDestination(@RequestBody Destination destination)  {
        if(destinationService.addDestination(destination) == 1){
            return ResponseEntity.status(HttpStatus.CREATED).build();
