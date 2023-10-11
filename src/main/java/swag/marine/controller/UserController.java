977f2c814d9a1874bff3aa6bd97d0384617d0b00
JaeUng <wodndrnjs@naver.com>
2023-10-11 17:19:09 +0900

    Swagger api doc 작성중 - User api 완료

--- src/main/java/swag/marine/controller/UserController.java
+++ src/main/java/swag/marine/controller/UserController.java
@@ -1,5 +1,11 @@
 package swag.marine.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +16,17 @@ import swag.marine.service.UserService;
 @RequestMapping("/marine/users")
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "users", description = "유저 관련 API")
 public class UserController {
     private final UserService userService;
+    
+    @Operation(summary = "유저 추가", description = "회원가입 절차를 통해 유저를 추가합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저를 정상적으로 등록했습니다.",
+                content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "유저 추가에 실패했습니다.",
+                content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("")
     public ResponseEntity<?> addUser(@RequestBody User user){
         boolean flag = userService.addUser(user);
@@ -19,17 +34,36 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("fail!");
     }
+    @Operation(summary = "유저 조회", description = "유저 아이디를 통해 유저 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저를 정상적으로 조회했습니다.",
+                content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = User.class)))
+    })
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUser(@PathVariable String userId){
         User user = userService.getUser(userId);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
+    @Operation(summary = "유저 수정", description = "유저 정보를 수정합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저를 정상적으로 수정했습니다.",
+                    content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = User.class)))
+    })
     @PutMapping("")
-    public ResponseEntity<User> updateUser(@RequestBody User user){
+    public ResponseEntity<?> updateUser(@RequestBody User user){
         boolean flag = userService.updateUser(user);
-        if(flag) return ResponseEntity.status(HttpStatus.OK).body(user);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(user);
+        if(flag) return ResponseEntity.status(HttpStatus.OK).body("success!");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("fail!");
     }
+    @Operation(summary = "로그인", description = "유저 아이디, 비밀번호를 입력받아 로그인합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인이 정상적으로 완료되었습니다.",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400",description = "로그인에 실패했습니다.",
+                    content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("/auth")
     public ResponseEntity<?> login(@RequestBody User user){
         boolean flag = userService.passwordCheck(user);
@@ -37,11 +71,18 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("fail!");
     }
+    @Operation(summary = "아이디 중복 검사", description = "유저 아이디룰 입력받아 아이디 중복을 확인합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "사용 가능한 아이디 입니다.",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400",description = "사용 중인 아이디입니다.",
+                    content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/duplicate/{userId}")
     public ResponseEntity<?> idDuplicateCheck(@PathVariable String userId){
         boolean flag = userService.idDuplicateCheck(userId);
 
-        if(flag) return ResponseEntity.status(HttpStatus.OK).body("duplicate!");
+        if(flag) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("duplicate!");
 
         return ResponseEntity.status(HttpStatus.OK).body("no duplicate!");
     }
