4f866fee2fa882f3975886e11b963b8491f1b3fd
JaeUng <wodndrnjs@naver.com>
2023-10-11 16:05:52 +0900

    Swagger api doc 작성중 - Model Schema 추가

--- src/main/java/swag/marine/controller/OrderController.java
+++ src/main/java/swag/marine/controller/OrderController.java
@@ -1,5 +1,11 @@
 package swag.marine.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +18,11 @@ import java.util.List;
 @RequestMapping("/marine/orders")
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "orders",description = "주문 관련 API")
 public class OrderController {
     private final OrderService orderService;
     @GetMapping("/{orderId}")
-    public ResponseEntity<Order>getOrderByOrderId(@PathVariable Integer orderId){
+    public ResponseEntity<Order> getOrderByOrderId(@PathVariable Integer orderId){
         return ResponseEntity.status(HttpStatus.OK).body(orderService.getOrdersByOrderId(orderId));
     }
     @GetMapping("/users/{userId}")
@@ -26,10 +33,18 @@ public class OrderController {
     public ResponseEntity<List<Order>> getOrderByStoreId(@PathVariable String storeId){
         return ResponseEntity.status(HttpStatus.OK).body(orderService.getOrdersByStoreId(storeId));
     }
+
+    @Operation(summary = "주문 추가",description = "주문을 추가합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "주문이 정상적으로 추가되었습니다.",
+            content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400",description = "주문 추가에 실패했습니다.",
+                    content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("")
     public ResponseEntity<?> addOrders(@RequestBody Order order){
         boolean flag = orderService.addOrders(order);
-        if(flag) return ResponseEntity.status(HttpStatus.OK).body("success!");
+        if(flag) return ResponseEntity.status(HttpStatus.CREATED).body("success!");
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("fail!");
     }
