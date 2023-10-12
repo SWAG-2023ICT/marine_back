bc6d7998da60df9226479944eecbb9c872f57e02
JaeUng <wodndrnjs@naver.com>
2023-10-12 14:07:51 +0900

    update password 변수명 변경

--- src/main/java/swag/marine/controller/UserController.java
+++ src/main/java/swag/marine/controller/UserController.java
@@ -70,8 +70,8 @@ public class UserController {
             @ApiResponse(responseCode = "400",description = "비밀번호 변경에 실패했습니다.")
     })
     @PutMapping("/password")
-    public ResponseEntity<?> updatePassword(String oldPassword, User user){
-        boolean flag = userService.updatePassword(oldPassword,user);
+    public ResponseEntity<?> updatePassword(String newPassword, User user){
+        boolean flag = userService.updatePassword(newPassword,user);
         if(flag) return ResponseEntity.status(HttpStatus.OK).body("success!");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("fail!");
     }
