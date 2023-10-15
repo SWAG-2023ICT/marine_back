34267b09948b6e43e5f6bd3c1c88fbe9355ae502
jaewoong-gwon <wodndrnjs@naver.com>
2023-10-15 14:46:48 +0900

    SecurityConfig 오타 수정

--- src/main/java/swag/marine/common/config/SecurityConfig.java
+++ src/main/java/swag/marine/common/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class SecurityConfig {
 
@@ -26,8 +28,7 @@ public class SecurityConfig {
     public CorsConfigurationSource configurationSource(){
         CorsConfiguration cors = new CorsConfiguration();
         cors.addAllowedOrigin("*");
-        cors.addAllowedOrigin("*");
-        cors.addAllowedMethod("GPT,POST,PUT,DELETE");
+        cors.setAllowedMethods(List.of("GET,POST,PUT,DELETE"));
         cors.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
